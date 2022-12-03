14f4016b9fb10746b2f7ece2ebbc3ce8413b2883
alihaydarsayar <alihaydarsyr@gmail.com>
2022-12-03 20:44:12 +0300

    add buttons

--- ToDo_App/script.js
+++ ToDo_App/script.js
@@ -4,7 +4,7 @@
 const input = document.querySelector('.todo-input');
 const inputId = document.getElementById('input');
 const button = document.querySelector('.todo-button');
-const list = document.querySelector('.todo-list');
+const todoList = document.querySelector('.todo-list');
 
 
 //
@@ -14,12 +14,25 @@ button.addEventListener('click', addTodo);
 function addTodo(event) {
     event.preventDefault(); // don't refresh
 
-
     const todoDiv = document.createElement('div'); // create div
     todoDiv.classList.add("todo");
+
     const todoLi = document.createElement('li');  // create li
     todoLi.innerText = 'hey';
     todoLi.classList.add("todo-item");
-    todoDiv.appendChild(todoLi);    
-    
+
+    todoDiv.appendChild(todoLi);
+
+    const completedButton = document.createElement('button');  // create completed button
+    completedButton.innerHTML = '<i class="fas fa-check"> </i>';
+    completedButton.classList.add("complate-btn");
+
+    todoDiv.appendChild(completedButton)
+
+    const deleteButton = document.createElement('button');  // create delete button
+    deleteButton.innerHTML = '<i class="fas fa-trash"> </i>';
+    deleteButton.classList.add("delete-btn");
+
+    todoDiv.appendChild(deleteButton)
+    todoList.appendChild(todoDiv); // append to list 
 }
