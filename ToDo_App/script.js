b24dd09909ba92e8c9cef46c4bdac888ac94ab20
alihaydarsayar <alihaydarsyr@gmail.com>
2022-12-03 20:16:02 +0300

    add div and li

--- ToDo_App/script.js
+++ ToDo_App/script.js
@@ -1,1 +1,25 @@
-"use script"
+"use strict" //
+
+//
+const input = document.querySelector('.todo-input');
+const inputId = document.getElementById('input');
+const button = document.querySelector('.todo-button');
+const list = document.querySelector('.todo-list');
+
+
+//
+button.addEventListener('click', addTodo);
+
+
+function addTodo(event) {
+    event.preventDefault(); // don't refresh
+
+
+    const todoDiv = document.createElement('div'); // create div
+    todoDiv.classList.add("todo");
+    const todoLi = document.createElement('li');  // create li
+    todoLi.innerText = 'hey';
+    todoLi.classList.add("todo-item");
+    todoDiv.appendChild(todoLi);    
+    
+}
